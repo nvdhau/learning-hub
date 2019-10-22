b3b76d64f2c0fecaf8405dbbafa1de94273f219b
Nguyen <nguyenv19@student.douglascollege.ca>
2019-10-22 10:40:25 -0700

    added mysql schema

---
+++ backend-node/database/mysqlSchema.sql
@@ -0,0 +1,66 @@
+-- email match with firebase authentication 
+DROP DATABASE IF EXISTS test;
+
+CREATE DATABASE test;
+USE test;
+CREATE TABLE USERS (
+    user_id VARCHAR(200) PRIMARY KEY,   
+    user_name VARCHAR(50),
+    full_name TEXT,
+    in_active BOOLEAN NOT NULL DEFAULT 0
+);
+
+CREATE TABLE POSTS (
+    post_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    author VARCHAR(200) NOT NULL,
+    title TEXT,
+    description TEXT,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted BOOLEAN NOT NULL DEFAULT 0,
+
+    FOREIGN KEY (author) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE RATING(
+    user_id VARCHAR(200),
+    post_id INTEGER,
+    rating TINYINT NOT NULL,
+    PRIMARY KEY (post_id, user_id),
+    FOREIGN KEY (post_id) REFERENCES POSTS(post_id),
+    FOREIGN KEY (user_id) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE ATTACHMENTS(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    type VARCHAR(100) NOT NULL,
+    url TEXT NOT NULL,
+    is_local BOOLEAN NOT NULL DEFAULT 1,
+
+    FOREIGN KEY (post_id) REFERENCES POSTS(post_id)
+);
+
+CREATE TABLE TAGS(
+    tag_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255),
+    tag_slug VARCHAR(255)
+); 
+
+CREATE TABLE POST_TAG(
+    post_id INTEGER,
+    tag_id INTEGER,
+    PRIMARY KEY (post_id, tag_id),
+    FOREIGN KEY (post_id) REFERENCES POSTS(post_id),
+    FOREIGN KEY (tag_id) REFERENCES TAGS(tag_id)
+);
+
+CREATE TABLE COMMENTS(
+    comment_id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    reply_id INTEGER,
+    content TEXT NOT NULL,
+
+    FOREIGN KEY (post_id) REFERENCES POSTS(post_id),
+    FOREIGN KEY (reply_id) REFERENCES COMMENTS(comment_id)
+);
