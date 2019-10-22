2a9a707a7640b52038f3f30ae590f5e51118d32f
quangle <quang.le205@gmail.com>
2019-10-21 22:09:02 -0700

    Set up Home Page and some reusable components which later on replaced by API Data

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './sass/App.scss';
+import { Component } from 'react';
+import HeaderNavigation from './components/HeaderNavigation';
+import MenuBar from './components/MenuBar';
+import Home from './components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Special Topic - Welcome to React</p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <HeaderNavigation />
+        <MenuBar />
+        <Home />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ react-frontend/src/components/MenuBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Component } from 'react';
+
+// import SCSS if any
+import '../sass/HeaderNavigation.scss';
+
+class MenuBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <React.Fragment>
+          <div className="main-menu menu-fixed menu-light menu-accordion menu-shadow" data-scroll-to-active="true">
+            <div className="main-menu-content">
+              <ul className="navigation navigation-main tags-menu" id="main-menu-navigation" data-menu="menu-navigation">
+                <li className="navigation-header"><span data-i18n="nav.category.admin-panels"><i className="la la-tags"></i><strong> # Tags</strong></span>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">csis4280</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">webapache</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">linux</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">studygroup</span></a>
+                </li>
+
+                <li className="navigation-header"><span data-i18n="nav.category.admin-panels"><i className="la la-users"></i><strong> # Follow up</strong></span>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">csis4280</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">webapache</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">linux</span></a>
+                </li>
+                <li className="nav-item tags"><a><span className="menu-title">studygroup</span></a>
+                </li>
+              </ul>
+            </div>
+          </div>  
+        </React.Fragment>
+    )
+  }
+}
+
+export default MenuBar;
