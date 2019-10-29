4e3986d8a1e5e8ccfa1a47c2d8d4a4eabd883038
Fershark <ferchriquelme@gmail.com>
2019-10-29 09:48:33 -0700

    List users and update user added

--- backend-node/models/User.js
+++ backend-node/models/User.js
@@ -18,10 +18,20 @@ class User extends BaseModel {
     return new User(row.id, row.username, row.full_name, Boolean(row.is_active));
   }
 
+  static get() {
+    return super.get()
+      .then(([rows]) => rows.map(row => this.fromDB(row)));
+  }
+
   static findBy(propertyName, value) {
     return super.findBy(propertyName, value)
-      .then((rows) => this.fromDB(rows[0]));
+      .then(rows => this.fromDB(rows[0]));
   } 
+
+  static update(user) {
+    return super.update(user)
+      .then(row => user);
+  }
 }
 
 module.exports = User;
